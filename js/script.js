ef3de1c73c6608f646456af83ac56f7754ec8f4b
Todd Beckman <toddbeckman1@gmail.com>
2015-09-15 23:38:53 -0600

    added profile data through json and ajax

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.debugging = false;   //  changes between localhost and web server
+
+window.ROOTURL = debugging ? "http://localhost:8080" : "http://sysci.xyz";
+
+// http://papermashup.com/read-url-get-variables-withjavascript/
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+    });
+    return vars;
+}
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
